231429d4ca1821543d7f1c01d2febdf22c09fd2a
Gabriel Cerbaro <gabrielis2one@yahoo.com>
2026-07-08 20:51:17 -0300

    add json middleware for request body parsing and create buffer example

--- src/server.js
+++ src/server.js
@@ -1,21 +1,12 @@
 import http from 'node:http'
+import { json } from './middlewares/json.js'
 
 const users = []
 
 const server = http.createServer(async (request, response) => {
     const {method, url} = request
 
-    const buffers = []
-
-    for await (const chunk of request) {
-        buffers.push(chunk)
-    }
-
-    try {
-        request.body = JSON.parse(Buffer.concat(buffers).toString())
-    }catch {
-        request.body = null
-    }
+    await json(request, response)
 
     if(method === 'GET' && url === '/users') {
         return response
